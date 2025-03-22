37ee0199d78f755c64767e48a2260f103f40a3f0
Jaemyeong Jin <jaemyeong@me.com>
2025-03-22 20:18:05 +0900

    refactor: 불필요한 코드 주석 제거 및 클래스 선언 수정

--- BusanBus/ViewController.swift
+++ BusanBus/ViewController.swift
@@ -1,19 +1,3 @@
-//
-//  ViewController.swift
-//  BusanBus
-//
-//  Created by 진재명 on 3/22/25.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
-}
-
+public final class ViewController: UIViewController {}
--- BusanBusTests/BusanBusTests.swift
+++ BusanBusTests/BusanBusTests.swift
@@ -1,17 +1,5 @@
-//
-//  BusanBusTests.swift
-//  BusanBusTests
-//
-//  Created by 진재명 on 3/22/25.
-//
-
 import Testing
+
 @testable import BusanBus
 
-struct BusanBusTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
-
-}
+public struct BusanBusTests {}
