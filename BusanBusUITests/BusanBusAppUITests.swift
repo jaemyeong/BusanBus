8d4dca7be8f5822dafc39fd6a321dc13eb193602
Jaemyeong Jin <jaemyeong@me.com>
2025-03-23 16:21:38 +0900

    chore: AppCoordinator 및 일부 테스트 파일 삭제, 새로운 UITest 파일 추가 및 파일 구조 변경

---
+++ BusanBusUITests/BusanBusAppUITests.swift
@@ -0,0 +1,5 @@
+import XCTest
+
+import AppUITests
+
+public final class BusanBusAppUITests: AppUITests {}
---
+++ Packages/App/Tests/AppUITests/AppUITests.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  App
+//
+//  Created by 진재명 on 3/23/25.
+//
+
+import Foundation
