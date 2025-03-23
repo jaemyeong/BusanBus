efb06230f002c7702100d900f96b010a017c8317
Jaemyeong Jin <jaemyeong@me.com>
2025-03-23 14:59:36 +0900

    feat: 앱 코디네이터 및 기본 UI 구성 추가, UI 테스트 케이스 작성 및 패키지 구조 설정

--- BusanBusUITests/BusanBusUITests.swift
+++ BusanBusUITests/BusanBusUITests.swift
@@ -1,3 +1,27 @@
 import XCTest
 
-public final class BusanBusUITests: XCTestCase {}
+public final class BusanBusUITests: XCTestCase {
+    
+    public override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        return true
+    }
+    
+    public override func setUp() {
+        super.setUp()
+        
+        self.continueAfterFailure = false
+    }
+    
+    public func testView() {
+        let app = XCUIApplication()
+        app.launch()
+        
+        let view = app.otherElements["View"]
+        XCTAssertTrue(view.exists)
+        
+        let textLabel = view.staticTexts["textLabel"]
+        XCTAssertTrue(textLabel.exists)
+        
+        XCTAssertEqual(textLabel.label, "Hello, World!")
+    }
+}
