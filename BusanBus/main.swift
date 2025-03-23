84cd839dd7474b5558e7a828d98af39631bd8a98
Jaemyeong Jin <jaemyeong@me.com>
2025-03-24 00:30:00 +0900

    feat: logging 기능 추가 및 Swift 패키지 의존성 설정 변경 (#20)

--- BusanBus/main.swift
+++ BusanBus/main.swift
@@ -1,4 +1,7 @@
 import UIKit
+import Logging
 import App
+
+let logger: Logger = Logger(label: #function)
 
 UIApplicationMain(CommandLine.argc, CommandLine.unsafeArgv, nil, NSStringFromClass(AppDelegate.self))
--- Packages/App/Sources/App/AppDelegate.swift
+++ Packages/App/Sources/App/AppDelegate.swift
@@ -1,7 +1,14 @@
 import UIKit
+import Logging
 import Core
 
 public final class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        logger.info("\(#function)")
+        
+        return true
+    }
     
     public func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         let configuration = UISceneConfiguration(name: UISceneConfiguration.defaultConfiguration.rawValue, sessionRole: connectingSceneSession.role)
